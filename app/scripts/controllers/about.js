82c5ee3bdf1f96fd290de29c61799cfb63e96989
Jagdeep Matharu <Jagdeepmatharu88@gmail.com>
2015-07-17 21:43:15 -0400

    Setting up Application

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name courseWareHubApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the courseWareHubApp
+ */
+angular.module('courseWareHubApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name courseWareHubApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the courseWareHubApp
+ */
+angular.module('courseWareHubApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
